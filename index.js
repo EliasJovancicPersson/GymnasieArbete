c2a4ceecadd5e9f8e23c2411baa5903ece1cd89f
Elias Jovancic Persson <EliasJovancicPersson@gmail.com>
2022-11-24 14:29:51 +0100

    added if check

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const dropdownNodelist = document.getElementsByClassName("dropdown");
-let dropdownArray = Array.from(dropdownNodelist);
+if (document.getElementsByClassName("dropdown")) {
+	const dropdownNodelist = document.getElementsByClassName("dropdown");
+	let dropdownArray = Array.from(dropdownNodelist);
 
-dropdownArray.forEach(function (element) {
-	console.log(element);
-	element.addEventListener("click", () => {
-		DropDown(element);
+	dropdownArray.forEach(function (element) {
+		console.log(element);
+		element.addEventListener("click", () => {
+			DropDown(element);
+		});
 	});
-});
+}
 
 function DropDown(element) {
 	let dropdownContent = element.querySelector(".dropdown-content");
