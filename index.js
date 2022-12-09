77583527752ca1b3aab6311212fae89ccaac1a11
Elias Jovancic Persson <EliasJovancicPersson@gmail.com>
2022-12-09 16:59:08 +0100

    renamed get function

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 if (document.getElementsByClassName("dropdown")) {
-  const dropdownNodelist = document.getElementsByClassName("dropdown");
-  let dropdownArray = Array.from(dropdownNodelist);
+	const dropdownNodelist = document.getElementsByClassName("dropdown");
+	let dropdownArray = Array.from(dropdownNodelist);
 
-  dropdownArray.forEach(function (element) {
-    element.addEventListener("click", () => {
-      DropDown(element);
-    });
-  });
+	dropdownArray.forEach(function (element) {
+		element.addEventListener("click", () => {
+			DropDown(element);
+		});
+	});
 }
 
 function DropDown(element) {
-  let dropdownContent = element.querySelector(".dropdown-content");
-  dropdownContent.classList.toggle("display-none");
+	let dropdownContent = element.querySelector(".dropdown-content");
+	dropdownContent.classList.toggle("display-none");
 }
 
 let url = "http://localhost:8000/wiki";
 
 let responseArray = [];
 
-async function Test() {
-  let responseVar = fetch(url, {
-    method: "GET",
-  })
-    .then((response) => response.json())
-    .then(async function (response) {
-      responseArray = await response.projects;
+async function GetProjects() {
+	let responseVar = fetch(url, {
+		method: "GET",
+	})
+		.then((response) => response.json())
+		.then(async function (response) {
+			responseArray = await response.projects;
 
-      if (document.querySelectorAll(".gyarb")) {
-        let titles = document.querySelector(".title .title-header h3");
+			if (document.querySelectorAll(".gyarb")) {
+				let titles = document.querySelector(".title .title-header h3");
 
-        let gyarbText = document.querySelector(".title-answer p");
+				let gyarbText = document.querySelector(".title-answer p");
 
-        titles.textContent = responseArray[0].title;
-        gyarbText.textContent = responseArray[0].text;
-      }
-    });
+				titles.textContent = responseArray[0].title;
+				gyarbText.textContent = responseArray[0].text;
+			}
+		});
 }
 
-Test();
+GetProjects();
