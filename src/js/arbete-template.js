b8f3554671b74674ac5610433f813b34e4d0a150
eliasjovancicpersson <eliasjovancicpersson@gmail.com>
2022-12-10 17:40:24 +0100

    fetching and creating work sites done

--- src/js/arbete-template.js
+++ src/js/arbete-template.js
@@ -20,10 +20,32 @@ const responseVar = fetch(fetchUrl, {
     );
   });
 
-function UpdateText(titel, author, timestamp, text, subject) {
-  console.log(titel);
+function UpdateText(title, author, timestamp, text, subject) {
+  console.log(title);
   console.log(author);
   console.log(timestamp);
   console.log(text);
   console.log(subject);
+  try {
+    //get titel
+    const titelElem = document.querySelector(".gyarb .text-info .title");
+    //get author
+    const authorElem = document.querySelector(".gyarb .text-info .author");
+    //get subject
+    const subjectElem = document.querySelector(".gyarb .text-info .subject");
+    //get timestamp
+    const timestampElem = document.querySelector(
+      ".gyarb .text-info .timestamp"
+    );
+    //get text
+    const textElem = document.querySelector(".gyarb .text-main");
+
+    titelElem.textContent = title;
+    authorElem.textContent = author;
+    subjectElem.textContent = subject;
+    timestampElem.textContent = timestamp;
+    textElem.textContent = text;
+  } catch (err) {
+    console.log("Could not get all elements :" + err);
+  }
 }
