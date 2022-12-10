36435e0ba5b166ffebe66855261abb6a87baa1d0
eliasjovancicpersson <eliasjovancicpersson@gmail.com>
2022-12-10 15:32:03 +0100

    fixed file paths

---
+++ src/js/index.js
@@ -0,0 +1,37 @@
+if (document.getElementsByClassName("dropdown")) {
+  const dropdownNodelist = document.getElementsByClassName("dropdown");
+  let dropdownArray = Array.from(dropdownNodelist);
+
+  dropdownArray.forEach(function (element) {
+    element.addEventListener("click", () => {
+      DropDown(element);
+    });
+  });
+}
+
+function DropDown(element) {
+  let dropdownContent = element.querySelector(".dropdown-content");
+  dropdownContent.classList.toggle("display-none");
+}
+
+let url = "http://localhost:8000/wiki";
+
+let responseArray = [];
+
+async function GetProjects() {
+  let responseVar = fetch(url, {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then(async function (response) {
+      responseArray = await response.projects;
+
+      if (document.querySelectorAll(".gyarb")) {
+        let titles = document.querySelector(".title .title-header h3");
+
+        titles.textContent = responseArray[0].title;
+      }
+    });
+}
+
+GetProjects();
