3673cc37a45d3c80f573d1374cc239f0f7e7ea64
eliasjovancicpersson <eliasjovancicpersson@gmail.com>
2022-12-12 01:45:58 +0100

    added a way to scroll pages with a 2d array

--- src/js/arbeten.js
+++ src/js/arbeten.js
@@ -1,8 +1,20 @@
 let url = "http://localhost:8000/wiki";
-
 let responseArray = [];
-
+let pages = [[]];
+let pagesIndex = 0;
+let currentPage = 0;
 const maxEntires = 10;
+
+const gyarb = document.querySelector(".work-list");
+const scrollBack = document.querySelector("#backwards");
+const scrollForw = document.querySelector("#forwards");
+
+scrollBack.addEventListener("click", function () {
+  scrollBackward();
+});
+scrollForw.addEventListener("click", function () {
+  scrollForward();
+});
 
 async function GetProjects() {
   let responseVar = fetch(url, {
@@ -13,60 +25,89 @@ async function GetProjects() {
       responseArray = await response.projects;
 
       if (document.querySelector(".gyarb")) {
-        CreateElements(responseArray);
-
-        try {
-          let a = document.querySelectorAll(".gyarb a");
-          let titles = document.querySelectorAll(".title .title-header h3");
-
-          for (let i = 0; i < a.length; i++) {
-            try {
-              a[i].id = responseArray[i]._id;
-              const pageUrl = "/src/html/work.html?id=" + a[i].id;
-              a[i].href = pageUrl;
-              titles[i].textContent = responseArray[i].title;
-            } catch (err) {
-              console.log(err);
-              //display error on website for not finding the project from backend
-              titles[i].textContent =
-                "ERROR, could not read from database. Maybe it dosent exist?";
-            }
-          }
-        } catch (err) {
-          console.log(err);
-          //display error on website for not finding the html elements
+        for (let i = 0; i < 11; i++) {
+          CreateLinkElement();
         }
+        DisplayPage(); //page can now be switched
+        UpdateText(currentPage);
       }
     });
 }
 
-function CreateElements(projectsArray) {
-  //create elements for each project, max 10 before new page
-  console.log(projectsArray);
-  const gyarb = document.querySelector(".gyarb");
+function UpdateText(page) {
+  try {
+    let a = document.querySelectorAll(".gyarb a");
+    let titles = document.querySelectorAll(".title .title-header h3");
 
-  const elementArray = [];
-
-  projectsArray.forEach((element) => {
-    if (elementArray.length < maxEntires) {
-      const a = document.createElement("a");
-      elementArray.push(a);
-      a.classList = "work-link";
-      const div0 = document.createElement("div");
-      div0.classList = "title";
-      const div1 = document.createElement("div");
-      div1.classList = "title-header";
-      const title = document.createElement("h3");
-      gyarb.appendChild(a);
-      a.appendChild(div0);
-      div0.appendChild(div1);
-      div1.appendChild(title);
-      console.log("created elements");
-    } else {
-      console.log("max enties reached");
+    for (let i = 0; i < a.length; i++) {
+      try {
+        a[i].id = responseArray[i + page * 10]._id;
+        const pageUrl = "/src/html/work.html?id=" + a[i].id;
+        a[i].href = pageUrl;
+        titles[i].textContent = responseArray[i + page * 10].title;
+      } catch (err) {
+        console.log(err);
+        //display error on website for not finding the project from backend
+        titles[i].textContent =
+          "ERROR, could not read from database. Maybe it dosent exist?";
+      }
     }
-  });
-  console.log(elementArray);
+  } catch (err) {
+    console.log(err);
+    //display error on website for not finding the html elements
+  }
+}
+
+function CreateLinkElement() {
+  //create elements for each project, max 10 before new page
+
+  const a = document.createElement("a");
+  a.classList = "work-link";
+  const div0 = document.createElement("div");
+  div0.classList = "title";
+  const div1 = document.createElement("div");
+  div1.classList = "title-header";
+  const title = document.createElement("h3");
+
+  a.appendChild(div0);
+  div0.appendChild(div1);
+  div1.appendChild(title);
+
+  if (pages[pagesIndex].length < maxEntires) {
+    pages[pagesIndex].push(a);
+  } else {
+    pagesIndex++;
+    pages[pagesIndex] = new Array();
+    pages[pagesIndex].push(a);
+  }
+}
+
+function DisplayPage() {
+  if (document.querySelectorAll(".work-link")) {
+    let elements = document.querySelectorAll(".work-link");
+    elements.forEach((element) => {
+      element.remove();
+    });
+  }
+
+  for (let i = 0; i < pages[currentPage].length; i++) {
+    gyarb.appendChild(pages[currentPage][i]);
+  }
+}
+
+function scrollForward() {
+  if (currentPage + 1 <= pagesIndex) {
+    currentPage++;
+    DisplayPage();
+    UpdateText(currentPage);
+  }
+}
+function scrollBackward() {
+  if (currentPage - 1 >= 0) {
+    currentPage--;
+    DisplayPage();
+    UpdateText(currentPage);
+  }
 }
 
 GetProjects();
