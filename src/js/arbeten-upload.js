e2d41d90dd6be876666f6d8b2528655bdff19ef1
Elias Jovancic Persson <EliasJovancicPersson@gmail.com>
2022-12-13 01:15:12 +0100

    comment

--- src/js/arbeten-upload.js
+++ src/js/arbeten-upload.js
@@ -9,32 +9,32 @@ const submitElement = document.querySelector("form .form-end input");
 submitElement.addEventListener("click", submit);
 
 function submit() {
-  if (
-    titleElement.value &&
-    authorElement.value &&
-    textElement.value &&
-    subjectElement.value != "Välj ett ämne"
-  ) {
-    console.log(titleElement.value);
-    post();
-  } else {
-    console.log("error, missing field");
-  }
+	if (
+		titleElement.value &&
+		authorElement.value &&
+		textElement.value &&
+		subjectElement.value != "Välj ett ämne"
+	) {
+		console.log(titleElement.value);
+		post();
+	} else {
+		console.log("error, missing field");
+	}
 }
 
 function post() {
-  fetch("http://localhost:8000/wiki", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: new URLSearchParams({
-      title: titleElement.value,
-      author: authorElement.value,
-      text: textElement.value,
-      subject: subjectElement.value,
-    }),
-  })
-    .then((response) => response.json())
-    .then((response) => console.log(JSON.stringify(response)));
+	fetch("http://localhost:8000/wiki", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded",
+		},
+		body: new URLSearchParams({
+			title: titleElement.value, //need to add function to optionally post images to azure storage
+			author: authorElement.value, //post with blob rest api
+			text: textElement.value,
+			subject: subjectElement.value,
+		}),
+	})
+		.then((response) => response.json())
+		.then((response) => console.log(JSON.stringify(response)));
 }
