e7575f8de889f73b1102db9bb614f664dea9e8c2
eliasjovancicpersson <eliasjovancicpersson@gmail.com>
2022-12-23 00:50:17 +0100

    nav bar fix

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,7 +1,7 @@
-const activePage = location.pathname;
+/*const activePage = location.pathname;
 if (activePage == "/src/html/work.html") {
 } else {
   const navElement = document.querySelector(`a[href^='${activePage}']`);
   console.log(activePage);
   navElement.classList.add("active");
-}
+}*/
