2606b9460b8288ea97328dcec3202708a301c70c
eliasjovancicpersson <eliasjovancicpersson@gmail.com>
2022-12-23 01:16:58 +0100

    more nav fixses,js

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,7 +1,15 @@
-/*const activePage = location.pathname;
+const searchBtn = document.querySelector("#search");
+searchBtn.addEventListener("click", Search);
+
+function Search() {
+  //query api to search
+}
+
+const activePage = location.pathname;
 if (activePage == "/src/html/work.html") {
 } else {
   const navElement = document.querySelector(`a[href^='${activePage}']`);
+  console.log(navElement);
   console.log(activePage);
   navElement.classList.add("active");
-}*/
+}
